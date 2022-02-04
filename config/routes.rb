ab26741ba4627f2d796991cac96255690dcc93cc
Taeju Park <philippark89@gmail.com>
2022-02-04 02:42:51 -0600

    7 CRUDit Where CRUDit Is Due

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "articles", to: "articles#index"
+  root "articles#index"
+
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
+  resources :articles
 
   # Defines the root path route ("/")
   # root "articles#index"
